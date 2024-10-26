9b6999fefd4cdadaf93bf8698d04769bf54c4c5d
Restor3b <Mikiszoko22@wp.pl>
2024-10-26 14:42:54 +0200

    ddd

--- src/commands/Utility/slahscommand-awans_degrad.js
+++ src/commands/Utility/slahscommand-awans_degrad.js
@@ -132,12 +132,11 @@ module.exports = new ApplicationCommand({
             newRankIndex = currentRankIndex + 1;
         }
 
-        const newRank = rankRoles[newRankIndex];
-        const channelId = '1299672680391245846';
+        
         try {
             await targetMember.roles.add(newRank.id);
             await targetMember.roles.remove(rankRoles.filter(role => role.id !== newRank.id).map(role => role.id));
-            await channel.send(`<@${target.id}>`);
+          
             const embed = new EmbedBuilder()
                 .setAuthor({ name: author.username, iconURL: author.displayAvatarURL() })
                 .setTitle(`Zmiana stopnia funkcjonariusza`)
@@ -156,26 +155,35 @@ module.exports = new ApplicationCommand({
                 .setColor(0x2f3136)
                 .setThumbnail('https://media.discordapp.net/attachments/1293717333461827747/1299497166158565457/f85cc66dd65a679d957ca4d6c668d070.png?ex=671d6a8b&is=671c190b&hm=6313f2ba968894d2e2382d3878ee0103179b9fc7209d30ff68451397c33cfdf1&=&format=webp&quality=lossless');
 
-            
-            const channel = client.channels.cache.get(channelId);
-            if (channel) {
-                await channel.send({ embeds: [embed] });
-                await interaction.reply({
-                    content: `Użytkownik został ${action === 'promote' ? 'awansowany' : 'zdegradowany'} na stopień ${newRank.name}.`,
-                    ephemeral: true
-                });
-            } else {
+            const channelId = '1299672680391245846';
+            let channel = client.channels.cache.get(channelId);
+            if (!channel) {
                 await interaction.reply({
                     content: 'Nie udało się znaleźć określonego kanału.',
                     ephemeral: true
                 });
+                return;
             }
-        } catch (err) {
-            console.error(err);
-            await interaction.reply({
-                content: 'Nie udało się zmienić rangi użytkownika.',
-                ephemeral: true
-            });
+                await channel.send(`<@${target.id}>`);
+                if (channel) {
+                    await channel.send({ embeds: [embed] });
+                    await interaction.reply({
+                        content: `Użytkownik został ${action === 'promote' ? 'awansowany' : 'zdegradowany'} na stopień ${newRank.name}.`,
+                        ephemeral: true
+                    });
+                } else {
+                    await interaction.reply({
+                        content: 'Nie udało się znaleźć określonego kanału.',
+                        ephemeral: true
+                    });
+                }
+            } catch (err) {
+                console.error(err);
+                await interaction.reply({
+                    content: 'Nie udało się zmienić rangi użytkownika.',
+                    ephemeral: true
+                });
+            }
         }
-    }
-}).toJSON();
+    }).toJSON();
+    
