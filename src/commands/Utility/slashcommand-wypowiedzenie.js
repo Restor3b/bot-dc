6bd5064c5b78f874039b208f3e404ffcd777d0af
Restor3b <Mikiszoko22@wp.pl>
2024-11-05 00:21:19 +0100

    id

--- src/commands/Utility/slashcommand-wypowiedzenie.js
+++ src/commands/Utility/slashcommand-wypowiedzenie.js
@@ -25,7 +25,7 @@ module.exports = new ApplicationCommand({
      * @param {ChatInputCommandInteraction} interaction 
      */
     run: async (client, interaction) => {
-        const requiredRoleId = '1299662554473435186';
+        const requiredRoleId = '1259796857291411558';
         const member = interaction.guild.members.cache.get(interaction.user.id);
         if (!member.roles.cache.has(requiredRoleId)) {
             await interaction.reply({
