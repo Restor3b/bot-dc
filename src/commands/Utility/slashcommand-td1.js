899be46551e2ceed750462698c6a24ccd47e4f1c
Restor3b <Mikiszoko22@wp.pl>
2024-11-22 15:40:16 +0100

    d

--- src/commands/Utility/slashcommand-td1.js
+++ src/commands/Utility/slashcommand-td1.js
@@ -101,8 +101,8 @@ module.exports = new ApplicationCommand({
                 { name: 'Szkoleniowiec: ', value: `<@${szkoleniowiec.id}>`},
                 { name: 'Rodzaj szkolenia: ', value: `${rodzajSzkolenia}`, inline: true },
                 { name: 'Wynik egzaminu: ', value: `${wynikOpis}`, inline: true },
-                { name: 'Data: ', value: `${dataSzkolenia}`, },
-                { name: 'Podpis: ', value: `${podpis}`, inline: true },
+                { name: 'Data: ', value: `${dataSzkolenia}`},
+                { name: 'Podpis: ', value: `${podpis}`},
                 { name: '**------------------------------------------------------------------**', value: ' '}
             )
             .setColor(0x2f3136);
