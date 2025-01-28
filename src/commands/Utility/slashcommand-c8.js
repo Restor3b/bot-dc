f055f70b3d4ae5f327625d0fc4e156340cf9160a
Restor3b <Mikiszoko22@wp.pl>
2025-01-28 21:59:13 +0100

    ss

--- src/commands/Utility/slashcommand-c8.js
+++ src/commands/Utility/slashcommand-c8.js
@@ -104,7 +104,7 @@ module.exports = new ApplicationCommand({
             try {
                 // Autoryzacja z użyciem pliku JSON
                 const auth = new google.auth.GoogleAuth({
-                    keyFile: '\src\commands\Utility\bot-dc-449215-43833ac2c28c.json', // Ścieżka do Twojego pliku z kluczem serwisowym
+                    keyFile: '..\Utility\bot-dc-449215-43833ac2c28c.json', // Ścieżka do Twojego pliku z kluczem serwisowym
                     scopes: ['https://www.googleapis.com/auth/spreadsheets'],
                 });
 
