78456759f4e1540b31b9f3a1fcffa702bb5b18fc
Restor3b <Mikiszoko22@wp.pl>
2025-07-23 23:23:18 +0200

    d

--- src/commands/Utility/slashcommand-zatrudnij.js
+++ src/commands/Utility/slashcommand-zatrudnij.js
@@ -1,4 +1,4 @@
-const { ChatInputCommandInteraction, EmbedBuilder, User } = require("discord.js");
+const { ChatInputCommandInteraction, EmbedBuilder} = require("discord.js");
 const DiscordBot = require("../../client/DiscordBot");
 const ApplicationCommand = require("../../structure/ApplicationCommand");
 
